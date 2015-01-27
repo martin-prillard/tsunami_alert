cebffddfaf395d3e992eb1a84df10214b57eab9b
guillaumeph <Guillaume@MacBook-Pro-de-Guillaume.local>
2015-01-27 15:47:16 +0100

    calculation of the nodes closets

--- src/util/util_geo.py
+++ src/util/util_geo.py
@@ -43,3 +43,13 @@ def get_GSM_codes_close_to_impact(latitude, longitude, km_range):
     GSMZone_dist = GSMZone_dist.sort(['dist_from_seism'],ascending=1)
     GSMZone_dist_500 = GSMZone_dist[GSMZone_dist['dist_from_seism'] <= km_range]
     return GSMZone_dist_500
+
+
+def get_nodes_close_to_impact(latitude, longitude, km_range):
+    Node_dist_to_impact = pd.read_csv('../../dataset/Nodes_locations.csv')
+    impact_coord = (latitude, longitude)
+    Node_dist_to_impact = Node_dist_to_impact.apply(lambda x : get_distance_to_seism(x, impact_coord), axis=1)
+    Node_dist_to_impact = Node_dist_to_impact.sort(['dist_from_seism'],ascending=1)
+    Closest_nodes = Node_dist_to_impact[0:2]
+    return Closest_nodes
+
