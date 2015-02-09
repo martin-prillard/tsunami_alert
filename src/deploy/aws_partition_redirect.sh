f22de8c29c47d7966bfa1ff1771a73565e4cb0e9
Martin Prillard <prillard.martin@gmail.com>
2015-02-09 22:47:18 +0100

    add deploy script

---
+++ src/deploy/aws_partition_redirect.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# ./aws_partition_redirect.sh myKey.pem 52.0.104.140 52.0.104.141 52.0.104.142
+
+if [ "$#" -lt "2" ]; then
+    echo "Illegal number of parameters"
+    echo "Example : ./aws_partition_redirect.sh myKey.pem 52.0.104.140 52.0.104.141 52.0.104.142"
+    echo "Note : first host is the master"
+    exit 1
+fi
+
+
+# get the pem file
+pem=$1
+shift
+# get the master host
+master=$1
+
+# launch initialize master script TODO enable
+# ssh -i $pem -o StrictHostKeyChecking=no ubuntu@$master 'bash -s' < 'initialize_master.sh'
+
+# initialize forwarding port for spark ui
+for (( p=0; p<=$#; p++ ))
+do
+	echo "ssh -L 808$p:127.0.0.1:808$p -nNT -i $pem -o StrictHostKeyChecking=no ubuntu@$master &"
+	ssh -L 808$p:127.0.0.1:808$p -nNT -i $pem -o StrictHostKeyChecking=no ubuntu@$master &
+done
+echo "ssh -L 4040:127.0.0.1:4040 -nNT -i $pem -o StrictHostKeyChecking=no ubuntu@$master &"
+ssh -L 4040:127.0.0.1:4040 -nNT -i $pem -o StrictHostKeyChecking=no ubuntu@$master &
+
+
+# redirect spark rdd directories for each hosts
+for host in "$@"
+do
+  echo "redirect spark rdd directories for $host"
+  ssh -i $pem -o StrictHostKeyChecking=no ubuntu@$host "sudo mkdir /raid0/spark/; sudo mv /var/lib/spark/* /raid0/spark/; sudo rmdir /var/lib/spark/; sudo ln -s /raid0/spark/ /var/lib/"
+done
+
+
+
---
+++ src/deploy/initialize_master.sh
@@ -0,0 +1,19 @@
+# install anaconda
+cd ~
+wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.9.2-Linux-x86_64.sh
+bash Anaconda-1.9.2-Linux-x86_64.sh -b
+cd ~
+echo "export PATH=/home/ubuntu/anaconda/bin:$PATH" >> .bashrc
+export PATH=/home/ubuntu/anaconda/bin:$PATH
+sudo apt-get install libev4 libev-dev
+
+# install modules
+cd ~
+pip install cassandra-driver
+pip install geopy
+pip install cqlengine
+sudo apt-get install redis-server
+
+# git clone tsunami project
+cd ~
+git clone https://github.com/martin-prillard/tsunami_alert.git
